f535dbaa3761c9283f31bb6cb7d26dd750ca3d10
ZLoo <Kozlov.s.v.1992@gmail.com>
2025-01-26 07:31:17 +0300

    done

--- main.nim
+++ main.nim
@@ -1,6 +1,27 @@
-# импортируйте нужные библиотеки (советуем strutils и sequtils)
+import strutils, sequtils
 
 proc main() =
-  # Реализуйте ваш код здесь
+  let n: int = parseInt(stdin.readLine)
+  var m: seq[int] = stdin.readLine.split(" ").mapIt(parseInt(it))
+  let k: int = parseInt(stdin.readLine)
+  var m1: seq[int]
+  var left, right, middle: int 
+  for i in 0 .. (n - 1):
+    m1.add(k - m[i])
+  var flag: bool = true
+  for i in 0 .. (n - 2):
+    left = i + 1
+    right = n - 1
+    while (left <= right) and flag:
+      middle = (right + left) div 2
+      if m1[i] < m[middle]:
+        right = middle - 1
+      elif m1[i] > m[middle]:
+        left = middle + 1
+      else:
+        echo m[i],' ', m[middle]
+        flag = false
+  if flag:
+    echo "none"
 
 main()
